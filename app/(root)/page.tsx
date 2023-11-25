da3cbc226cdf419ff6a44ff609ea0e0c17830d29
Krriishhhhhh <krishgargakio@gmail.com>
2023-11-25 19:29:28 +0530

    modal provider added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,25 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
+"use client";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
+
 
 const SetUpPage = ()=> {
+
+  const onOpen = useStoreModal((state)=> state.onOpen)
+  const isOpen = useStoreModal((state)=> state.isOpen)
+  
+  useEffect(()=>{
+    if(!isOpen){
+      onOpen();
+    }
+  },[isOpen,onOpen])
+  
     return (
       <div className="p-4">
-        <UserButton afterSignOutUrl="/"/>
+        Root page
       </div>
-    )
+    );
   }
 
 export default SetUpPage;  
