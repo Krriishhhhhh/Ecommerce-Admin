3388ea660998165ed651997b7bc61d5dfcd9df57
Krriishhhhhh <krishgargakio@gmail.com>
2023-12-01 14:44:11 +0530

    Dark Mode added and  project finalized

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,25 +1,61 @@
-import primsadb from '@/lib/prismadb'
+import { CreditCard, DollarSign, Package } from "lucide-react";
 
-interface DashboardPageProps{
-    params : {storeId : string}
-}
+import { Separator } from "@/components/ui/separator";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Heading } from "@/components/ui/heading";
+import { formatter } from "@/lib/utils";
 
-const DashboardPage:React.FC<DashboardPageProps> = async ({
-    params
-})=>{
+interface DashboardPageProps {
+  params: {
+    storeId: string;
+  };
+};
 
-    const store = await primsadb.store.findFirst({
-        where:{
-            id : params.storeId
-        }
-    })
-    
-    return(
-        
-        <>
-        <div>Active Store : {store?.name}</div>
-        </>
-    );
-}
+const DashboardPage: React.FC<DashboardPageProps> = async ({ 
+  params
+}) => {
+
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <Heading title="Dashboard" description="Overview of your store" />
+        <Separator />
+        <div className="grid gap-4 grid-cols-3">
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">
+                Total Revenue
+              </CardTitle>
+              <DollarSign className="h-4 w-4 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">{formatter.format(10000)}</div>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">Sales</CardTitle>
+              <CreditCard className="h-4 w-4 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">+{97}</div>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">Products In Stock</CardTitle>
+              <Package className="h-4 w-4 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">{16}</div>
+            </CardContent>
+          </Card>
+        </div>
+       
+      </div>
+    </div>
+  );
+};
 
 export default DashboardPage;
